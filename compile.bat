0c8644823a7b0e9eaaeb4b85f9f8f87d91a344ff
Your Name <you@example.com>
2013-05-15 23:11:53 +0900

    ok

---
+++ compile.bat
@@ -0,0 +1,8 @@
+G:\texlive\2011\bin\win32\platex.exe thesis
+G:\texlive\2011\bin\win32\bibtex.exe thesis
+G:\texlive\2011\bin\win32\platex.exe thesis
+G:\texlive\2011\bin\win32\bibtex.exe thesis
+G:\texlive\2011\bin\win32\platex.exe thesis
+G:\texlive\2011\bin\win32\bibtex.exe thesis
+G:\texlive\2011\bin\win32\platex.exe thesis
+G:\texlive\2011\bin\win32\dvipdfm.exe thesis
