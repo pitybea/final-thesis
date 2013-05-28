e262b6029b5cae39ca73cc774c01a9af80fb1554
pitybea <wangzhp85@gmail.com>
2013-05-28 21:12:58 +0900

    ok

--- compile.bat
+++ compile.bat
@@ -6,3 +6,4 @@ G:\texlive\2011\bin\win32\platex.exe thesis
 G:\texlive\2011\bin\win32\bibtex.exe thesis
 G:\texlive\2011\bin\win32\platex.exe thesis
 G:\texlive\2011\bin\win32\dvipdfm.exe thesis
+foxit thesis.pdf
