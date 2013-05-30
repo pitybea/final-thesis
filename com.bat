6a7116971247fbcf7e09f570b8cfc3590adc1b3a
Your Name <you@example.com>
2013-05-30 15:25:02 +0900

    ok

---
+++ com.bat
@@ -0,0 +1,9 @@
+E:\texlive\bin\win32\platex.exe thesis
+E:\texlive\bin\win32\bibtex.exe thesis
+E:\texlive\bin\win32\platex.exe thesis
+E:\texlive\bin\win32\bibtex.exe thesis
+E:\texlive\bin\win32\platex.exe thesis
+E:\texlive\bin\win32\bibtex.exe thesis
+E:\texlive\bin\win32\platex.exe thesis
+E:\texlive\bin\win32\dvipdfm.exe thesis
+"C:\Program Files (x86)\Adobe\Acrobat 9.0\Acrobat\Acrobat.exe"  thesis.pdf 
