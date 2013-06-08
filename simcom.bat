6ca853992df9829f7db54177a1fc74c56bbac456
pitybea <wangzhp85@gmail.com>
2013-06-08 20:00:49 +0900

    ok

--- simcom.bat
+++ simcom.bat
@@ -1,9 +1,7 @@
-E:\texlive\bin\win32\platex.exe thesis
-E:\texlive\bin\win32\bibtex.exe thesis
-E:\texlive\bin\win32\platex.exe thesis
-E:\texlive\bin\win32\bibtex.exe thesis
-E:\texlive\bin\win32\platex.exe thesis
-E:\texlive\bin\win32\bibtex.exe thesis
-E:\texlive\bin\win32\platex.exe thesis
-E:\texlive\bin\win32\dvipdfm.exe thesis
-"C:\Program Files (x86)\Adobe\Acrobat 9.0\Acrobat\Acrobat.exe"  thesis.pdf 
+H:\texlive\2011\bin\win32\platex.exe thesis
+H:\texlive\2011\bin\win32\dvipdfm.exe thesis
+pdftk thesis.pdf cat 3-end output tem1.pdf
+del thesis.pdf
+pdftk cover.pdf tem1.pdf cat output thesis.pdf
+del tem1.pdf
+foxit thesis.pdf
