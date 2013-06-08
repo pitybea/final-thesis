59cbdabb675f6768bf3665e29d2b1012a52a19f2
Your Name <you@example.com>
2013-06-08 12:19:42 +0900

    ok

--- com.bat
+++ com.bat
@@ -6,4 +6,8 @@ E:\texlive\bin\win32\platex.exe thesis
 E:\texlive\bin\win32\bibtex.exe thesis
 E:\texlive\bin\win32\platex.exe thesis
 E:\texlive\bin\win32\dvipdfm.exe thesis
+pdftk thesis.pdf cat 3-end output tem1.pdf
+del thesis.pdf
+pdftk cover.pdf tem1.pdf cat output thesis.pdf
+del tem1.pdf
 "C:\Program Files (x86)\Adobe\Acrobat 9.0\Acrobat\Acrobat.exe"  thesis.pdf 
